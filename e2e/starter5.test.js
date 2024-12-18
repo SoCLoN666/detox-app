930aa893dde4561b0bc9d20e6b971cf8afc16d90
SoCLoN666 <kusminoleh96@gmail.com>
2024-12-19 00:21:56 +0200

    new test 2

--- e2e/starter5.test.js
+++ e2e/starter5.test.js
@@ -19,4 +19,17 @@ describe("Example", () => {
     await element(by.text("Home")).tap();
     await expect(element(by.text("Welcome!"))).toBeVisible();
   });
+
+  it("should be able to open Explore tab 2", async () => {
+    await element(by.text("Explore")).tap();
+
+    await expect(
+      element(by.text("This app includes example code to help you get started."))
+    ).toBeVisible();
+  });
+
+  it("should show hello screen after tap 3", async () => {
+    await element(by.text("Home")).tap();
+    await expect(element(by.text("Welcome!"))).toBeVisible();
+  });
 });
