9f40d38344a1ef1360f5b72d6714e0955a2d5ddf
OlehKusmin <olehkus@teampro.com>
2025-06-25 17:12:36 +0300

    fix: add architecture

--- e2e/visual-test.test.js
+++
@@ -1,5 +0,0 @@
-// it.skip("Terms and conditions screen should match snapshot", async () => {
-//   await device.launchApp();
-//   await device.reloadReactNative(); // Optional: reload the app
-//   await jestExpect("Terms and conditions").toMatchImageSnapshot();
-// });
